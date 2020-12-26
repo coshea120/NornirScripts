f8da09c60dbf99df9e77c0dbd225cde56951f539
coshea120 <christian.oshea82@gmail.com>
2020-12-26 12:18:57 -0500

    Update config_snmp.py

--- config_snmp.py
+++ config_snmp.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python
+
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command, netmiko_send_config
+from nornir_utils.plugins.functions import print_result
+
+nornir_instance = InitNornir(config='config.yaml')
