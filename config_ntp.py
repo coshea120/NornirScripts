5898041e934f91dead5f484b8e91019895776bdf
coshea120 <christian.oshea82@gmail.com>
2020-12-26 11:15:46 -0500

    Update config_ntp.py

--- config_ntp.py
+++ config_ntp.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command, netmiko_send_config
+from nornir.plugins.functions.text import print_result, print_title
+
+nr = InitNornir(config="config.yaml", dry_run=True)
+result = nornir.run(netmiko_send_command, command_string="ntp server " + nr.defaults['ntp_server'])
+print_result(result)
