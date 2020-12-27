de56ff0bd872a46adb9ac07b5c7cadc8eedaf612
coshea120 <christian.oshea82@gmail.com>
2020-12-27 09:42:26 -0500

    Added host entries to hosts.yaml Updated scripts from nornir v2 to v3

--- config_ntp.py
+++ config_ntp.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 from nornir import InitNornir
-from nornir.plugins.tasks.networking import netmiko_send_command, netmiko_send_config
-from nornir.plugins.functions.text import print_result, print_title
+from nornir_netmiko import netmiko_send_command, netmiko_send_config
+from nornir_utils.plugins.functions import print_result, print_title
 
 nr = InitNornir(config="config.yaml", dry_run=True)
-result = nornir.run(netmiko_send_command, command_string="ntp server " + nr.defaults['ntp_server'])
+result = nr.run(netmiko_send_command, command_string="ntp server " + nr.defaults['ntp_server'])
 print_result(result)
--- config_snmp.py
+++ config_snmp.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 from nornir import InitNornir
-from nornir.plugins.tasks.networking import netmiko_send_command, netmiko_send_config
-from nornir.plugins.functions.text import print_result, print_title
+from nornir_netmiko import netmiko_send_command, netmiko_send_config
+from nornir_utils.plugins.functions import print_result, print_title
 
 nr = InitNornir(config="config.yaml", dry_run=True)
 snmp_config = nr.defaults['snmp']
-result = nornir.run(netmiko_send_command, command_string="snmp community " + snmp_config['comm_str'])
+result = nr.run(netmiko_send_command, command_string="snmp community " + snmp_config['comm_str'])
 print_result(result)
